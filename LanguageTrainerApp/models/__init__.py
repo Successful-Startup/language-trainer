572f55fbb86fb3af6e863e4bdd08b1ee2490d78a
Aleksandr Gnatiuk <alex-gnatuyk@yandex.ru>
2024-07-12 21:59:41 +0200

    Add word controller

--- LanguageTrainerApp/models/__init__.py
+++ LanguageTrainerApp/models/__init__.py
@@ -7,3 +7,5 @@ from .adjective import Adjective
 from .word_form import WordForm
 from .adjective_form import AdjectiveForm
 from .phrase import Phrase
+
+__all__ = ['Gender', 'PartOfSpeech', 'WordNumber', 'Case', 'Word', 'Adjective', 'WordForm', 'AdjectiveForm', 'Phrase']
---
+++ LanguageTrainerApp/serializers/__init__.py
@@ -0,0 +1,3 @@
+from .word_serializser import WordSerializer
+
+__all__ = ['WordSerializer']
