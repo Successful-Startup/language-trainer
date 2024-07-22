39cc9f3be2c61e2a4a937c5fbc9c869da9ed5fe2
Aleksandr Gnatiuk <alex-gnatuyk@yandex.ru>
2024-07-22 21:10:44 +0200

    Fix URL for words endpoint

--- language_trainer_app/urls/urls.py
+++ language_trainer_app/urls/urls.py
@@ -1,10 +1,10 @@
 # Desc: URL configuration for the language_trainer_app
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from language_trainer_app.controllers import WordViewSet
 
-router = SimpleRouter(trailing_slash=False)
-router.register(r'words', WordViewSet)
+router = DefaultRouter(trailing_slash=False)
+router.register(r'', WordViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
