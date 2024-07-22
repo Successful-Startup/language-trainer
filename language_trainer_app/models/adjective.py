94f218568659099280c3373e6e592d957af2cd41
Aleksandr Gnatiuk <alex-gnatuyk@yandex.ru>
2024-07-22 22:32:53 +0200

    Change model's ids from int to uuid

--- language_trainer_app/models/adjective.py
+++ language_trainer_app/models/adjective.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class Adjective(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     adjective_nom = models.CharField(max_length=100)
     part_of_speech = models.ForeignKey('PartOfSpeech', on_delete=models.CASCADE)
 
