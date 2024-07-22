03bacf5389700bf48cd2bba8ed94b654f14bcaf3
Aleksandr Gnatiuk <alex-gnatuyk@yandex.ru>
2024-07-22 22:32:53 +0200

    Change model's ids from int to uuid

--- language_trainer_app/models/phrase.py
+++ language_trainer_app/models/phrase.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class Phrase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phrase_start = models.CharField(max_length=200)
     valid_words = models.ManyToManyField('Word')
 
