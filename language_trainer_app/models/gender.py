5a70ca894e531ea1774eb2491992c7891f94329a
Aleksandr Gnatiuk <alex-gnatuyk@yandex.ru>
2024-07-23 21:48:37 +0200

    Revert "Change model's ids from int to uuid"
    This reverts commit e3120c6a643a888b103554462089409a72a8ab51.

--- language_trainer_app/models/gender.py
+++ language_trainer_app/models/gender.py
@@ -1,8 +1,6 @@
-import uuid
 from django.db import models
 
 class Gender(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
 
     def __str__(self):
--- language_trainer_app/models/word.py
+++ language_trainer_app/models/word.py
@@ -1,8 +1,6 @@
-import uuid
 from django.db import models
 
 class Word(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     word_nom = models.CharField(max_length=100)
     gender = models.ForeignKey('Gender', on_delete=models.SET_NULL, null=True)
     part_of_speech = models.ForeignKey('PartOfSpeech', on_delete=models.CASCADE)
