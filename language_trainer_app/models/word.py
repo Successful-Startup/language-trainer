6bc0b38972175ac562c39037eb2241d3c93ca3c0
Aleksandr Gnatiuk <alex-gnatuyk@yandex.ru>
2024-07-23 22:03:33 +0200

    Remove reduntant models and tables

--- language_trainer_app/models/word.py
+++ language_trainer_app/models/word.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Word(models.Model):
-    word_nom = models.CharField(max_length=100)
+    base_form = models.CharField(max_length=100)
     gender = models.ForeignKey('Gender', on_delete=models.SET_NULL, null=True)
     part_of_speech = models.ForeignKey('PartOfSpeech', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.word_nom
+        return self.base_form
