3f007d807fc3ef0fadad1d16071edd6abe4bd420
Aleksandr Gnatiuk <alex-gnatuyk@yandex.ru>
2024-07-29 21:14:09 +0200

    Add infrastructure for part of speech

--- language_trainer_app/controllers/__init__.py
+++ language_trainer_app/controllers/__init__.py
@@ -1,3 +1,4 @@
 from .word_controler import WordViewSet
 from .gender_controller import GenderViewSet
 from .case_controller import CaseViewSet
+from .part_of_speech_controller import PartOfSpeechViewSet
