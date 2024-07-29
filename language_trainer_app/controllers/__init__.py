33cdcd07278906a3c0c56dbbec9c23778a391f0a
Aleksandr Gnatiuk <alex-gnatuyk@yandex.ru>
2024-07-29 21:36:33 +0200

    Add infrastructure for word number

--- language_trainer_app/controllers/__init__.py
+++ language_trainer_app/controllers/__init__.py
@@ -2,3 +2,4 @@ from .word_controler import WordViewSet
 from .gender_controller import GenderViewSet
 from .case_controller import CaseViewSet
 from .part_of_speech_controller import PartOfSpeechViewSet
+from .word_number_controller import WordNumberViewSet
