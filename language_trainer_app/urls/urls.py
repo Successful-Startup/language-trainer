5e853cbc75312bb5605c2a9881776ae639b7f281
crock0il <konovalovns@elocont.ru>
2024-08-14 21:25:06 +0500

    Add url for word forms

--- language_trainer_app/urls/urls.py
+++ language_trainer_app/urls/urls.py
@@ -6,6 +6,7 @@ from language_trainer_app.controllers.gender_controller import GenderViewSet
 from language_trainer_app.controllers.case_controller import CaseViewSet
 from language_trainer_app.controllers.part_of_speech_controller import PartOfSpeechViewSet
 from language_trainer_app.controllers.word_number_controller import WordNumberViewSet
+from language_trainer_app.controllers.word_form_controller import WordFormViewSet
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'words', WordViewSet)
@@ -13,6 +14,7 @@ router.register(r'genders', GenderViewSet)
 router.register(r'cases', CaseViewSet)
 router.register(r'partOfSpeech', PartOfSpeechViewSet)
 router.register(r'wordNumber', WordNumberViewSet)
+router.register(r'wordForm', WordFormViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
