b15a9b6ba73d3cc5b26fe8fdb3788bf3d795f56c
Aleksandr Gnatiuk <alex-gnatuyk@yandex.ru>
2026-04-05 15:14:13 +0200

    fix: refactor return statement for clarity in filter_queryset_casefold_exact function

--- language_trainer_app/utils/unicode_matching.py
+++ language_trainer_app/utils/unicode_matching.py
@@ -19,10 +19,14 @@ def filter_queryset_casefold_exact(queryset, field_name, search_term):
             casefold_ids.append(object_id)
 
     matching_ids = exact_ids + casefold_ids
-    return queryset.filter(id__in=matching_ids).annotate(
-        unicode_match_rank=Case(
-            When(id__in=exact_ids, then=Value(0)),
-            default=Value(1),
-            output_field=IntegerField(),
+    return (
+        queryset.filter(id__in=matching_ids)
+        .annotate(
+            unicode_match_rank=Case(
+                When(id__in=exact_ids, then=Value(0)),
+                default=Value(1),
+                output_field=IntegerField(),
+            )
         )
-    ).order_by("unicode_match_rank", "-id")
+        .order_by("unicode_match_rank", "-id")
+    )
